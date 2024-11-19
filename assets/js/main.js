f1734d6285301627104dd023c7e431d2af9fa9f3
laureschaeffer <laure.schaeffer@hotmail.com>
2024-11-19 16:35:30 +0100

    add scroll up, scroll reveal animation, differentes breakpoints for the responsive

--- assets/js/main.js
+++ assets/js/main.js
@@ -48,10 +48,52 @@ const swiperPopular = new Swiper('.popular__swiper', {
   });
 
 /*=============== SHOW SCROLL UP ===============*/ 
+const scrollUp = () =>{
+    const scrollUpEl = document.getElementById('scroll-up')
+    // When the scroll is higher than 350 viewport height, show the show-croll element
+    
+    this.scrollY >= 350 ? scrollUpEl.classList.add('show-scroll')
+                        : scrollUpEl.classList.remove('show-scroll')
+
+}
+window.addEventListener('scroll', scrollUp) 
 
 
 /*=============== SCROLL SECTIONS ACTIVE LINK ===============*/
+const sections = document.querySelectorAll('section[id]') //every section having an id
+    
+const scrollActive = () =>{
+  	const scrollDown = window.scrollY
 
+	sections.forEach(current => {
+		const sectionHeight = current.offsetHeight,
+			  sectionTop = current.offsetTop - 58,
+			  sectionId = current.getAttribute('id'),
+			  sectionsClass = document.querySelector('.nav__menu a[href*=' + sectionId + ']')
+        
+
+		if(scrollDown > sectionTop && scrollDown <= sectionTop + sectionHeight){
+			sectionsClass.classList.add('active-link')
+		} else {
+			sectionsClass.classList.remove('active-link')
+		}                                                    
+	})
+}
+window.addEventListener('scroll', scrollActive)
 
 /*=============== SCROLL REVEAL ANIMATION ===============*/
+const sr = ScrollReveal({
+    origin: 'top',
+    distance: '60px',
+    duration: 2500,
+    delay: 300,
+    // reset: true,  //  animations repeat 
+})
 
+sr.reveal(`.home__data, .popular__container, .footer`)
+sr.reveal(`.home__board`, {delay: 700, distance: '100px', origin: 'right'})
+sr.reveal(`.home__pizza`, {delay: 1400, distance: '100px', origin: 'bottom', rotate: {z: -90}})
+sr.reveal(`.home__ingredient`, {delay: 2000, interval: 100})
+sr.reveal(`.about__data, .recipe__list, .contact__data`, {origin: 'right'})
+sr.reveal(`.about__img, .recipe__img, .contact__image`, {origin: 'left'})
+sr.reveal(`.products__card`, {interval: 100})
